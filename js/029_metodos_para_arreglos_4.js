cd1c92d62740cb547122958b1936ccc93e1640c0
Marlon Melara <110437919+marlonmelara@users.noreply.github.com>
2023-10-18 18:57:14 -0300

    Update example file for array methods .include ()

---
+++ js/029_metodos_para_arreglos_4.js
@@ -0,0 +1,20 @@
+/*
+ * 📌 .include()
+ * - Nos permite saber si el arreglo contiene un elemento especificado
+ */
+
+const nombres = [
+  "Charlie",
+  "Snoppy",
+  "Woodstock",
+  "Lucy",
+  "Sally",
+  "Linus",
+  "Patty",
+  "Marcie",
+  "Snoppy",
+  "Schroeder",
+  "Franklin",
+];
+
+console.log(nombres.includes("Lucy"));
