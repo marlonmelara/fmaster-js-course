10e44711c751f30e0da833a0fccb047be086bc80
Marlon Melara <110437919+marlonmelara@users.noreply.github.com>
2023-10-21 18:21:57 -0300

    Update example file for object methods

--- js/030_metodos_para_objetos.js
+++ js/030_metodos_para_objetos.js
@@ -15,6 +15,17 @@ const usuario = {
 usuario.saludo();
 
 /*
- * 📌 Object.keys
+ * 📌 Object.keys();
  * Nos devuelve un arreglo con las llaves (keys) del objecto.
  */
+
+const resultadosPropiedades = Object.keys(usuario);
+console.log(resultadosPropiedades);
+
+/*
+ * 📌 Object.values();
+ * Nos devuelve un arreglo con las valores (values) del objecto.
+ */
+
+const resultadosValues = Object.values(usuario);
+console.log(resultadosValues);
